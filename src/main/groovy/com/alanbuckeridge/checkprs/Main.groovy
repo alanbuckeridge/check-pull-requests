cbdbcc88d13f496c9e02edf67d97c5ceeaec2b11
alanbuckeridge <alanbuckeridge@gmail.com>
2015-12-02 11:19:31 +0000

    Add colour to output to highlight when rebase needed, and when reviewers approve

--- src/main/groovy/com/alanbuckeridge/checkprs/Main.groovy
+++ src/main/groovy/com/alanbuckeridge/checkprs/Main.groovy
@@ -3,6 +3,9 @@ package com.alanbuckeridge.checkprs
  * abuckeridge
  */
 
+ANSI_RESET = "\u001B[0m"
+ANSI_RED = "\u001B[31m"
+ANSI_GREEN = "\u001B[32m"
 
 def config = new ConfigSlurper().parse(new File("config.properties").toURI().toURL())
 
@@ -16,12 +19,12 @@ prs.each { pr ->
     if (State.OPEN == pr.state) {
         println "Approvals:"
         if (pr.reviewers) {
-            pr.reviewers.each { r -> println "\t${r.name}: ${r.approved}" }
+            pr.reviewers.each { r -> println "\t${r.name}: ${greenIfTrue(r.approved)}" }
         }
         else {
             println "\t(No reviewers assigned)"
         }
-        println "Needs rebase? ${pr.mergeInfo.rebaseNeeded}"
+        println "Needs rebase? ${redIfTrue(pr.mergeInfo.rebaseNeeded)}"
         println "Ready for merge? ${pr.mergeInfo.readyForMerge}"
         pr.mergeInfo.vetoes.each {
             println "* ${it}"
@@ -29,5 +32,19 @@ prs.each { pr ->
     }
 
     println '-' * 80
+}
+
+private String redIfTrue(final boolean value) {
+    return colourIfTrue(value, ANSI_RED)
+}
+
+private String greenIfTrue(final boolean value) {
+    return colourIfTrue(value, ANSI_GREEN)
+}
+
+private String colourIfTrue(final boolean value, final String colour) {
+    if (value)
+        return colour + value.toString() + ANSI_RESET
+    return value.toString()
 }
 
